b96fa8156d58312f7a87a50c3d110956cc7bb834
farisrfp <farisrafp@gmail.com>
2024-11-30 09:24:37 +0700

    disable hdc1080

--- src/XDev32.cpp
+++ src/XDev32.cpp
@@ -3,63 +3,57 @@
 
 #include "XDev32.h"
 
-XDev32::XDev32():isInited(0) {
-
+XDev32::XDev32() : isInited(0) {
 }
 
-void XDev32::begin(bool SerialEnable, bool Sim7000Enable){
-	
-	//! Correct init once
-	if (isInited) return;
-	else isInited = true;
-
-	//! UART
-	if (SerialEnable) {
-		Serial.begin(115200);
-		Serial.flush();
-		delay(50);
-		Serial.println("X-Dev32 Initializing...");
-	}
-
-    hdc1080.Init(Temperature_Resolution_14_bit,Humidity_Resolution_14_bit);
-    Serial.println("HDC1080 Ready!"); 
-
-    if(veml6030.begin())
-    Serial.println("VEML6030 Ready!"); 
+void XDev32::begin(bool SerialEnable, bool Sim7000Enable) {
+    //! Correct init once
+    if (isInited)
+        return;
     else
-    Serial.println("Could not communicate with the VEML6030!");
-	veml6030.setGain(.125);
-	veml6030.setIntegTime(100);
+        isInited = true;
+
+    //! UART
+    if (SerialEnable) {
+        Serial.flush();
+        delay(50);
+        Serial.println("X-Dev32 Initializing...");
+    }
+
+    if (veml6030.begin())
+        Serial.println("VEML6030 Ready!");
+    else
+        Serial.println("Could not communicate with the VEML6030!");
+    veml6030.setGain(.125);
+    veml6030.setIntegTime(100);
 
     mpu6050.Init();
-    Serial.println("MPU6050 Ready!"); 
+    Serial.println("MPU6050 Ready!");
 
-	rgbled.Begin(XD_WS2812B, 1, RMT_PIXEL_RGB);
-	delay(25);
-	rgbled.FillLED(0,1,0,0,0);
+    rgbled.Begin(XD_WS2812B, 1, RMT_PIXEL_RGB);
+    delay(25);
+    rgbled.FillLED(0, 1, 0, 0, 0);
     rgbled.Write();
 
-	pinMode(XD_LED, OUTPUT);
-	digitalWrite(XD_LED, LOW);
+    pinMode(XD_LED, OUTPUT);
+    digitalWrite(XD_LED, LOW);
 
-	if (Sim7000Enable) {
-		pinMode(XD_SIM7000_RST, OUTPUT);
-  	    //digitalWrite(XD_SIM7000_RST, LOW);
-  		//delay(100);
-  		digitalWrite(XD_SIM7000_RST, HIGH);
-  		delay(200);
-	}else{
-		pinMode(XD_SIM7000_RST, OUTPUT);
-  	    digitalWrite(XD_SIM7000_RST, LOW);
-  	    delay(200);
-	}
-
+    if (Sim7000Enable) {
+        pinMode(XD_SIM7000_RST, OUTPUT);
+        // digitalWrite(XD_SIM7000_RST, LOW);
+        // delay(100);
+        digitalWrite(XD_SIM7000_RST, HIGH);
+        delay(200);
+    } else {
+        pinMode(XD_SIM7000_RST, OUTPUT);
+        digitalWrite(XD_SIM7000_RST, LOW);
+        delay(200);
+    }
 }
 
 void XDev32::update() {
-	XD.Btn.read();
-	XD.hdc1080.Get();
-	XD.mpu6050.Get();
+    Btn.read();
+    mpu6050.Get();
 }
 
 XDev32 XD;
--- src/XDev32.h
+++ src/XDev32.h
@@ -6,37 +6,37 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+
 #include "devices/Button.h"
 #include "devices/HDC1080.h"
-#include "devices/VEML6030.h"
 #include "devices/MPU6050.h"
 #include "devices/RMT_Pixel.h"
+#include "devices/VEML6030.h"
 
-#define XD_LED          32
-#define XD_BUTTON       35
-#define XD_SIM7000_PWR  5
-#define XD_SIM7000_RST  15
-#define XD_SIM7000_TXD  16
-#define XD_SIM7000_RXD  17
-#define XD_SIM7000_RI   18
-#define XD_SIM7000_DTR  19
-#define XD_WS2812B      14
+#define XD_LED 32
+#define XD_BUTTON 35
+#define XD_SIM7000_PWR 5
+#define XD_SIM7000_RST 15
+#define XD_SIM7000_TXD 16
+#define XD_SIM7000_RXD 17
+#define XD_SIM7000_RI 18
+#define XD_SIM7000_DTR 19
+#define XD_WS2812B 14
 
-#define USE_SERIAL      Serial
+#define USE_SERIAL Serial
 
 #define DEBOUNCE_MS 10
 
 class XDev32 {
-
- public:
+   public:
     XDev32();
-	void begin(bool SerialEnable=true, bool Sim7000Enable=false);
+    void begin(bool SerialEnable = true, bool Sim7000Enable = false);
     void update();
-
 
     Button Btn = Button(XD_BUTTON, true, DEBOUNCE_MS);
 
-    HDC1080 hdc1080;
+    /* ver 1.2 board is not working */
+    // HDC1080 hdc1080;
 
     VEML6030 veml6030 = VEML6030(altAddr);
 
@@ -44,16 +44,15 @@ class XDev32 {
 
     RMTPixelType rgbled;
 
- private:
+   private:
     bool isInited;
 };
-
 
 extern XDev32 XD;
 #define xd XD
 
 #else
-#error “This library only supports boards with ESP32 processor.”	
+#error “This library only supports boards with ESP32 processor.”
 #endif
 
 #endif
